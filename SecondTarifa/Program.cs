4be8b12411e395ccb5ee733a46e90087b4c2a6c0
markandmark1111@gmail.com <markandmark1111@gmail.com>
2020-06-09 16:16:10 +0200

    Add project files.

---
+++ SecondTarifa/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SecondTarifa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://open.kattis.com/problems/tarifa
+
+            var first = int.Parse(Console.ReadLine());
+            var second = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            int[] thirds = new int[second];
+            for (int i = 0; i < thirds.Length; i++)
+            {
+                thirds[i] = int.Parse(Console.ReadLine());
+                sum = sum + thirds[i];
+            }
+
+            var res = NextMonth(first, second, sum);
+            Console.WriteLine(res);
+        }
+        private static int NextMonth(int a, int b, int s)
+        {
+            return (a * (b + 1) - s);
+        }
+    }
+}
